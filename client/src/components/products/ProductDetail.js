881f81ef8b228d7b3b618997167f762dacbfa8b7
jonbae <jonbae61596@gmail.com>
2020-04-29 23:41:12 -0400

    enhance product detail

--- client/src/components/products/ProductDetail.js
+++ client/src/components/products/ProductDetail.js
@@ -1,9 +1,27 @@
 import React from "react";
-// import { Query } from "react-apollo";
-// import { Link } from "react-router-dom";
+import { Query } from "react-apollo";
+import { FETCH_PRODUCT } from "../../graphql/queries";
 
-const ProductDetail = () => {
-  return <div>ProductDetail</div>;
+// import { useParams } from "react-router-dom";
+// import { useParams } from "react-router";
+const ProductDetail = ({ match }) => {
+  const { productId } = match.params;
+  console.log(productId);
+  //   const test = { _id: id };
+  return (
+    <Query query={FETCH_PRODUCT} variables={{ _id: productId }}>
+      {({ loading, error, data }) => {
+        if (loading) return "Loading...";
+        if (error) return `Error! ${error.message}`;
+        return (
+          <div>
+            {data.product.name} - {data.product.description}
+            <p>weight: {data.product.weight}</p>
+          </div>
+        );
+      }}
+    </Query>
+  );
 };
 
 export default ProductDetail;
